38407e804fba80a3bef25976a4340b4c57841456
Mizz <123146044+MrMizzles@users.noreply.github.com>
2023-04-27 15:40:22 -0400

    Replaced JS code w/improved code, styled buttons
    Added HTML heading and created an unordered list: Pokémon-list.
     Replaced document.write with more efficient addListItem() function in the IIFE.  Created a button for each Pokémon in the array.  Added an event listener to the button.  When the button is clicked it will log Pokémon details in the console.  Added styling to the button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,74 @@
 /* Use comments often as it helps everyone, including yourself! */
-// IIFE starts
-let pokemonRepository = (function () {
-  
+// IIFE starts.
+var pokemonRepository = (function () {
+ 
   // Array for storing Pokémon.  
   let pokemonList = [
     {name:  "Bulbasaur", height: 2, weight: 15.2, type: ['Grass ', ' Poison']},
     {name: "Charzard", height: 5, weight: 199.5, type: ['Fire ', ' Flying']},
     {name: "Mewtwo", height: 6, weight: 269, type: ['Physhic']}];
-    
-  // Functions to add Pokémon to and retrieve from Pokémon list. 
-  // keywords to make functions accessible outside IIFE.
+  
+  // Checks validity and adds valid Pokémon to Pokémon array. 
   return {
     add: function (pokemon) {
-      if (typeof pokemon === "object" && "name" in pokemon)
+      if (typeof pokemon === "object" && "name" in pokemon && "height" in pokemon && "weight" in pokemon && "type" in pokemon)
     {
       pokemonList.push(pokemon);
     } else {
         console.log("invalid Pokémon")
     }},
+    
+    // Retrieves Pokémon array.
     getAll: function () {
       return pokemonList;
     },
+   
+    // Creates a button for each Pokémon in the array.
+    addListItem: function (pokemon) {
+      let pokemonList = document.querySelector('.pokemon-list');
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('button-class');
+      listItem.appendChild(button);
+      pokemonList.appendChild(listItem);
+
+      // When clicked logs Pokémon details in console.
+      button.addEventListener('click', function () {
+        showDetails(pokemon);
+      });
+    },
   };
 
-    // Makes functions accessible outside IIFE.
+  function showDetails(pokemon) {
+    console.log(pokemon)
+  };
+
+
+    // Keywords to make functions accessible outside IIFE.
     // return {
     //   add: add,
     //   getAll: getAll,
-    //   filterItems: filterItems,
+      // filterItems: filterItems,
+    //   addListItem: addListItem
     // };
 
 })();  // IIFE ENDS.
 
+console.log(pokemonRepository.getAll());
+
+//adds Pokémon to PokémonList array
 pokemonRepository.add(
   { 
     name: 'Pikachu', 
     height: 1.04, 
     weight: 13.2, 
-    type: ['Electric'],
-    category: ' Licking' 
+    type: ['Electric'], 
   });
 
 console.log(pokemonRepository.getAll());
 
-document.write("" + "Pokédex" + "<br>");
+//calls addListItem function
 pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write( 
-      " " +
-      pokemon.name +
-      " -" +
-      " " +
-      "(Height" +
-      ": " +
-      pokemon.height +
-      ")," +
-      " " +
-      "(Weight" +
-      ": " +
-      pokemon.weight +
-      ")," +
-      " " +
-      "(Type" +
-      ": " +
-      pokemon.type +
-      ")," +
-      " " +
-      "(Category" +
-      ": " +
-      pokemon.category +
-      ") <br>"
-  );
+  pokemonRepository.addListItem(pokemon);
 });
